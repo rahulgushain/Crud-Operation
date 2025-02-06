e5b7035f0c9109dc25818d081c8d15e0969ab72b
rahulgushain <151875743+rahulgushain@users.noreply.github.com>
2025-02-06 11:51:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import ContextFun from './components/context.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import {StrictMode} from 'react';
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
 <BrowserRouter>
   <ContextFun>
     <App />
     <Toaster/>
   </ContextFun>
   </BrowserRouter>
+    </StrictMode>
 
 )
